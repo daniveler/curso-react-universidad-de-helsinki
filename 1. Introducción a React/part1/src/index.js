3a913b78015c0a0ccfa2fbb71e48e70541b8eaf8
daniveler <daniel01velerdas@gmail.com>
2023-09-22 20:20:08 +0200

    Finished Exercise 1.1

--- 1. Introducción a React/part1/src/index.js
+++ 1. Introducción a React/part1/src/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
+const Header = (props) => {
   return (
     <div>
       <p>
+        <h1>Welcome to the course: {props.course}</h1>
+      </p>
+    </div>
+  )
+}
 
-        Hello {props.name}, you are {props.age} years old
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        <h2>{props.part1}</h2>
+        <p>Exercises: {props.exercises1}</p>
+        <h2>{props.part2}</h2>
+        <p>Exercises: {props.exercises2}</p>
+        <h2>{props.part3}</h2>
+        <p>Exercises: {props.exercises3}</p>
       </p>
     </div>
   )
@@ -15,21 +29,29 @@ const Hello = (props) => {
 const Footer = () => {
   return (
     <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+      <br/> <br/>
+      <p>
+      greeting app created by <a href="https://github.com/daniveler">daniveler</a>
+      </p>
     </div>
   )
 }
 
 const App = () => {
 
-  const name = 'Peter'
-  const age = 10
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3}
+        exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <Footer />
     </>
   )

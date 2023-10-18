52be735b860a3385cff9a65ff4ada886c63a5875
daniveler <daniel01velerdas@gmail.com>
2023-10-18 16:50:53 +0200

    Modified formularios

--- 2. Comunicación con el servidor/formularios/src/components/App.js
+++ 2. Comunicación con el servidor/formularios/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Note from './Note'
-import axios from 'axios'
-import noteService from './services/notes'
+import noteService from '../services/notes.js'
 
 
 const App = () => {
@@ -13,14 +12,17 @@ const App = () => {
         noteService
           .getAll()
           .then(response => {
-            setNotes(response.data)
+            setNotes(response)
           })
       }, [])
       
     const handleNoteChange = (event) => {
-        // console.log(event.target.value)
         setNewNote(event.target.value)
     }
+
+    const notesToShow = showAll 
+        ? notes
+        : notes.filter(note => note.important)
 
     const addNote = (event) => {
         event.preventDefault()
@@ -31,29 +33,30 @@ const App = () => {
             id: notes.length + 1,
         }
 
-        axios
-            .post('http://localhost:3001/notes', noteObject)
+        noteService
+            .create(noteObject)
             .then(response => {
                 setNotes(notes.concat(response.data))
                 setNewNote('')
-            })   
+            })
     }
-
-    const notesToShow = showAll 
-        ? notes
-        : notes.filter(note => note.important === true)
 
     const toggleImportanceOf = id => {
         const note = notes.find(n => n.id === id)
         const changedNote = { ...note, important: !note.important }
     
         noteService
-            .create(noteObject)
+            .update(id, changedNote)
             .then(response => {
-            setNotes(notes.concat(response.data))
-            setNewNote('')
+                setNotes(notes.map(note => note.id !== id ? note : response.data))
             })
-        }
+            .catch(error => {
+                alert(
+                    `the note '${note.content}' was already deleted from server`
+                )
+                setNotes(notes.filter(n => n.id !== id))
+            })
+    }
 
     return (
         <div>

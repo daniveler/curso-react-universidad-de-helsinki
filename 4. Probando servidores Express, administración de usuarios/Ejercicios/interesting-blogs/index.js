fbe104914f201e9d8164ccad605c4902134f007b
daniveler <daniel01velerdas@gmail.com>
2023-12-26 14:05:00 +0100

    Finished 4.1

--- 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/index.js
+++ 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -11,9 +13,17 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -30,14 +40,21 @@ app.get('/api/blogs', (request, response) => {
 app.post('/api/blogs', (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
+  // If the body is not valid
+  // if (!blog.title || !blog.author || !blog.url || !blog.likes) {
+  //   response.status(400).json({ error: 'Incorrect body' })
+  // }
+  // else {
+    blog
     .save()
     .then(result => {
       response.status(201).json(result)
     })
+  // }
+
+  
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })

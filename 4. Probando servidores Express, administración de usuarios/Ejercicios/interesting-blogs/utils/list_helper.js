593e1f45385abeae7aa303980285095d3e758312
daniveler <daniel01velerdas@gmail.com>
2024-01-04 11:39:03 +0100

    Finished 4.6

--- 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/utils/list_helper.js
+++ 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/utils/list_helper.js
@@ -22,8 +22,6 @@ const favoriteBlog = (blogs) => {
   // Get an array of all likes
   let likesList = blogs.map(blog => blog.likes)
 
-  console.log(likesList)
-
   // Return the blog which has the maximum likes
   return blogs.find(blog => blog.likes === Math.max(...likesList))
 } 
@@ -33,7 +31,27 @@ const mostBlogs = (blogs) => {
     return 0
   }
 
+  let authorList = []
+  
+  blogs.forEach(blog => {
+    // Search the index of the author in the author list
+    let index = authorList.findIndex(author => author.author === blog.author)
 
+    // If it's found, we increment his number of blogs
+    if (index !== -1) {
+      authorList[index].blogs++
+    }
+    // If not, we add it as a new author
+    else {
+      authorList.push({ author: blog.author, blogs: 1 })
+    }
+  })
+
+  // Get an array of all blogs
+  let blogsNumList = authorList.map(author => author.blogs)
+
+  // Return the author which has the maximum blogs
+  return authorList.find(author => author.blogs === Math.max(...blogsNumList))
 }
 
 module.exports = {

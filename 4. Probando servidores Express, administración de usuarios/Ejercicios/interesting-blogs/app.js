ec529e225ddcb4b159f6c88ec4b092368b5faead
daniveler <daniel01velerdas@gmail.com>
2024-01-05 20:23:08 +0100

    In progress 4.15

--- 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/app.js
+++ 4. Probando servidores Express, administración de usuarios/Ejercicios/interesting-blogs/app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const usersRouter = require('./controllers/users')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

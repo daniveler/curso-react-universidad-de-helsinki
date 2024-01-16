6cb0f1213d17c9f5946ce0006415e634e092565b
daniveler <daniel01velerdas@gmail.com>
2024-01-16 14:42:38 +0100

    Finished 5.9

--- 5. Probando aplicaciones React/Ejercicios/interesting-blogs/src/components/BlogsList.jsx
+++ 5. Probando aplicaciones React/Ejercicios/interesting-blogs/src/components/BlogsList.jsx
@@ -1,13 +1,13 @@
 import Blog from './Blog' 
 
 const BlogsList = (props) => {
-  
+  const orderedBlogs = (blogs) => blogs.sort((a, b) => b.likes - a.likes)
+
   return (
     <>
       <h2>Blogs List</h2>
-
       <div>
-        {props.blogs.map(blog =>
+        {orderedBlogs(props.blogs).map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
       </div>

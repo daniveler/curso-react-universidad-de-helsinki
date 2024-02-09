4f06c7d1d027f2f9c4f89746a78e75d09aed89f4
daniveler <daniel01velerdas@gmail.com>
2024-02-09 11:29:21 +0100

    Updated blog_app.cy.js

--- 5. Probando aplicaciones React/Ejercicios/interesting-blogs/cypress/e2e/blog_app.cy.js
+++ 5. Probando aplicaciones React/Ejercicios/interesting-blogs/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,16 @@
+const backendBaseUrl = 'http://localhost:3003'
+
+const testUser = {
+  name: 'Daniel Velerdas',
+  username: 'daniveler',
+  password: 'examplepassword'
+}
+
 describe('Interesting Blogs App', () => {
   beforeEach(() => {
+    cy.request('POST', `${backendBaseUrl}/api/testing/reset`)
+    
+    cy.request('POST', `${backendBaseUrl}/api/users`, testUser)
     cy.visit('http://localhost:3000')
   })
 
@@ -12,8 +23,8 @@ describe('Interesting Blogs App', () => {
   })
 
   it('user can log in', () => {
-    cy.get('#username').type('daniveler')
-    cy.get('#password').type('examplepassword')
+    cy.get('#username').type(testUser.username)
+    cy.get('#password').type(testUser.password)
     cy.get('#login-button').click()
 
     cy.contains('Daniel Velerdas logged in')
